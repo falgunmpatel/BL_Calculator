fab6dfdb83d65fc496571adc5fa6827729dfca99
Falgun Patel <falgunmpatel9123@gmail.com>
2025-01-17 11:59:20 +0530

    Welcome to calculator

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+// Driver class
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        double num1, num2;
+
+        System.out.println("Enter the numbers:");
+        num1 = sc.nextDouble();
+        num2 = sc.nextDouble();
+
+        System.out.println("Enter the operator (+,-,*,/):");
+
+        char op = sc.next().charAt(0);
+        double result = 0;
+
+        switch (op) {
+            default:
+                System.out.println("You enter wrong input");
+        }
+
+        System.out.println("The final result:");
+        System.out.println();
+
+        System.out.println(num1 + " " + op + " " + num2
+                + " = " + result);
+    }
+}
